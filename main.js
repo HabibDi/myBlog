43176f4faadcccdd81d27776a7dba70f5e97712c
HabibDi <habibdiallo78@gmail.com>
2022-11-21 00:32:30 +0100

    continuity on event listeners

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
-function carrousel() {
-    let leftArrow = document.getElementById('left_arrow')
-    let rigthArrow = document.getElementById('right_arrow')
+document.addEventListener("DOMContentLoaded", () => {
 
-leftArrow.onclick()
+    const leftArrow = document.getElementById("left_arrow")
+    const rightArrow = document.getElementById('right_arrow')
+    let items = document.getElementsByClassName('carrousel_items')
 
-}
+    function carrousel() {
+
+        leftArrow.addEventListener(onclick, () => {
+            console.log("left ok")
+        })
+
+        rightArrow.addEventListener(onclick, () => {
+            console.log("right ok")
+        })
+
+        console.log(items)
+
+    }
+
+    function selectedCourses() {
+
+
+    }
+
+    carrousel()
+    selectedCourses()
+
+});
+
+
