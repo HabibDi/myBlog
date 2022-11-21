4c91c741bc1b9b21d113312b2622533f68bd8c04
dhabib <habibdiallo78@gmail.com>
2022-11-21 15:54:04 +0100

    peer review

--- main.js
+++ main.js
@@ -1,20 +1,66 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const leftArrow = document.getElementById("left_arrow")
-    const rightArrow = document.getElementById('right_arrow')
-    let items = document.getElementsByClassName('carrousel_items')
+
 
     function carrousel() {
+        const leftArrow = document.getElementById("left_arrow")
+        const rightArrow = document.getElementById('right_arrow')
+        let itemsCollection = document.getElementsByClassName('carrousel_items')
+        let itemsArray = Array.from(itemsCollection)
 
-        leftArrow.addEventListener(onclick, () => {
-            console.log("left ok")
+
+        leftArrow.addEventListener("click", () => {
+            itemsArray.forEach(item => {
+                console.log(item)
+
+                if (item === itemsArray[0]) {
+
+                    itemsArray[0].setAttribute('hidden', '')
+                    itemsArray[2].removeAttribute('hidden')
+
+
+                } else if (item === itemsArray[1]) {
+
+                    itemsArray[1].setAttribute('hidden', '')
+                    itemsArray[0].removeAttribute('hidden')
+
+                } else if (item === itemsArray[2]) {
+
+                    itemsArray[2].setAttribute('hidden', '')
+                    itemsArray[1].removeAttribute("hidden")
+
+                }
+
+            })
         })
 
-        rightArrow.addEventListener(onclick, () => {
-            console.log("right ok")
-        })
+        rightArrow.addEventListener("click", () => {
 
-        console.log(items)
+            itemsArray.forEach(item => {
+
+
+                if (item === itemsArray[0]) {
+                    console.log(item)
+
+                    itemsArray[0].setAttribute('hidden', '')
+                    itemsArray[1].removeAttribute('hidden')
+
+
+                } else if (item === itemsArray[1]) {
+
+                    itemsArray[1].setAttribute('hidden', '')
+                    itemsArray[2].removeAttribute('hidden')
+
+                } else if (item === itemsArray[2]) {
+
+                    itemsArray[2].setAttribute('hidden', '')
+                    itemsArray[0].removeAttribute("hidden")
+
+                }
+
+
+            })
+        })
 
     }
 
